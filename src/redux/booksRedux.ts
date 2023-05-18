7862dcce489af6ce91531fb587796bfdabef9505
sicArchitect <maciej.kusztal1995@gmail.com>
2023-05-18 21:36:28 +0300

    Prepadre type and actions in Redux

--- src/redux/booksRedux.ts
+++ src/redux/booksRedux.ts
@@ -1,15 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Book } from "../components/interfaces/Book.interface";
 
-const initialState: any[] = [];
+const initialState: Book[] = [
+  {
+    id: "te1314derw",
+    title: "Of Mice and Men",
+    author: "John Steinbeck",
+    price: 25,
+  },
+  {
+    id: "t32s14dzdw",
+    title: "East of Eden",
+    author: "John Steinbeck",
+    price: 30,
+  },
+];
 
 const booksSlice = createSlice({
   name: "books",
   initialState,
   reducers: {
-    addBook(state, action) {
+    addBook(state, action: PayloadAction<Book>) {
       state.push(action.payload);
     },
-    removeBook(state, action) {
+    removeBook(state, action: PayloadAction<string>) {
       const index = state.findIndex((book) => book.id === action.payload);
       state.splice(index, 1);
     },
